6c66e6370a3125bfb51485a24adb5194a611fa10
adioz <adiozdaniel@gmail.com>
2025-06-21 11:27:18 +0300

    feat: updated user/urls.py with profile routes
    - Added route for user profile page: path('', views.profile, name='profile')
    - Added route for public user profiles: path('<str:username>', views.public_profile, name='public_profile')

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,7 @@
+from . import views
 from django.urls import path
-from django.contrib.auth import views
-
-from .forms import UserLoginForm
-from . import views as v
 
 urlpatterns = [
-    path('', v.register, name='register'),
-    path('company/', v.CompanySignUpView.as_view(), name='register_company'),
-    path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
-    path('login/', v.LoginUserView, name='login_user')
+    path('', views.profile, name='profile'),
+    path('<str:username>', views.public_profile, name='public_profile'),
 ]
