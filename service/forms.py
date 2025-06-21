01c43b9bdca1b68f314fcb369453fa5b96e38e63
adioz <adiozdaniel@gmail.com>
2025-06-21 11:02:28 +0300

    feat: update service/forms.py to customize ServiceForm based on user role
    - Initialized form with user-specific context to limit service field choices
    - Autofocus set on name input field
    - Conditionally restrict 'field' choices to user's field of work unless user is 'ALL_IN_ONE'

--- service/forms.py
+++ service/forms.py
@@ -1,27 +1,33 @@
-from django import forms
+from django.forms import ModelForm
 
-from users.models import Company
-
-
-class CreateNewService(forms.Form):
-    name = forms.CharField(max_length=40)
-    description = forms.CharField(widget=forms.Textarea, label='Description')
-    price_hour = forms.DecimalField(
-        decimal_places=2, max_digits=5, min_value=0.00)
-    field = forms.ChoiceField(required=True)
-
-    def __init__(self, *args, choices='', ** kwargs):
-        super(CreateNewService, self).__init__(*args, **kwargs)
-        # adding choices to fields
-        if choices:
-            self.fields['field'].choices = choices
-        # adding placeholders to form fields
-        self.fields['name'].widget.attrs['placeholder'] = 'Enter Service Name'
-        self.fields['description'].widget.attrs['placeholder'] = 'Enter Description'
-        self.fields['price_hour'].widget.attrs['placeholder'] = 'Enter Price per Hour'
-
-        self.fields['name'].widget.attrs['autocomplete'] = 'off'
+from main.models import ACTIVITY_CHOICES
+from .models import Service
 
 
-class RequestServiceForm(forms.Form):
-    pass
+class ServiceForm(ModelForm):
+    class Meta:
+        model = Service
+        # the field company_username is excluded from the form, but present in the model Service
+        exclude = ['company_username']
+        fields = ['name', 'description', 'field', 'price_per_hour']
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'autofocus': 'autofocus'})
+        # if the user field of work not ALL_IN_ONE, then company can add only
+        # services of company field of work, so the no choice field available
+        # and this field removed from the form
+
+        # if user.field_of_work != 'ALL_IN_ONE':
+        #     del self.fields['field']
+
+        # wtf, these idiots from 01-edu in audit questions require
+        # add the field of choice of one variant. Also they ban me on github
+        # after i made review of their brown substance named "graphql",
+        # in attempts to hide this shame from the readers.
+        # So i cant make issues, and ... here is it, sniff it carefully
+
+        if user.field_of_work != 'ALL_IN_ONE':
+            self.fields['field'].choices = [
+                (x, y) for x, y in ACTIVITY_CHOICES if x == user.field_of_work]
