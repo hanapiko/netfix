0d37cf72c4845915e7070680c066fc8506cbf802
adioz <adiozdaniel@gmail.com>
2025-06-21 10:59:43 +0300

    feat: update services app configuration
    - Updated services/app.py with default_auto_field as 'django.db.models.BigAutoField'
    - Set app name to 'service'

--- services/apps.py
+++ services/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class ServicesConfig(AppConfig):
-    name = 'services'
+class ServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'service'
