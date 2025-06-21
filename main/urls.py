56ce4e665cd71dc9c59d72483ecfd11efe403eff
adioz <adiozdaniel@gmail.com>
2025-06-21 10:43:57 +0300

    feat(urls): add routes for customer and company registration, user login, logout, filtered services, and most requested services view

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,15 @@
+from . import views
 from django.urls import path
-from . import views as v
 
-app_name = "main"
 
 urlpatterns = [
-    path('', v.home, name='home'),
-    path('logout/', v.logout, name='logout')
+    path('', views.home_view, name="home"),
+    # for filter/GARDENING/ the "GARDENING" is passed as "service_field" argument to "home_view"
+    path('filter/<str:service_field>/', views.home_view, name="home_with_filter"),
+    path('register/', views.register_view, name="register"),
+    path('register_customer/', views.register_customer, name="register_customer"),
+    path('register_company/', views.register_company, name="register_company"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('most_requested/', views.most_requested_view, name="most")
 ]
