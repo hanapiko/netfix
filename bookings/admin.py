db6ac489fe29875c925391d6c885902703dc50e0
hanapiko <apikojuma94@gmail.com>
2025-08-09 11:36:40 +0300

    Add Bookings admin interface with filtering and search

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from .models import Bookings
+
+@admin.register(Bookings)
+class BookingsAdmin(admin.ModelAdmin):
+    list_display = ['service', 'user', 'price', 'booking_date', 'address']
+    list_filter = ['booking_date', 'service__field']
+    search_fields = ['user__username', 'service__name', 'address']
+    readonly_fields = ['booking_date']
