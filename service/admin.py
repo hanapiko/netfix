572e8afba84f401d614ef264448e261a828026c3
hanapiko <apikojuma94@gmail.com>
2025-08-09 11:35:47 +0300

    Add Service admin interface with list display and search

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Service
 
-# Todo: Register models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'field', 'price_per_hour', 'company_username', 'created_date']
+    list_filter = ['field', 'created_date']
+    search_fields = ['name', 'description', 'company_username__username']
+    readonly_fields = ['created_date']
